5679e1803d2d03194911c660c512b1e7902dec3a
Colby <colby.butler.home@gmail.com>
2024-02-27 14:35:27 -0600

    don't track .vscode

--- cs.py
+++ cs.py
@@ -1,7 +1,8 @@
+values = ["fish", "bat", "shark", "zombie", "48 pounds of iron ore", "oil tanker full of crude oil"]
 
+new_list = []
 
-
-letters = "hello"
-numbers = [ord(letter) - ord('a') for letter in letters]
-total = sum(numbers)
-print(numbers)
+for value in values:
+    values.remove(value)
+    new_list.append(value)
+    print(values, '\n', new_list, '\n')
